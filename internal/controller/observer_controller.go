a8750dea245e3a637d297e58d731971013aff8f5
Fish-pro <zechun.chen@daocloud.io>
2023-07-27 15:42:02 +0800

    Complete the core logic of the controller
    Signed-off-by: Zechun Chen <zechun.chen@daocloud.io>

--- internal/controller/observer_controller.go
+++ internal/controller/observer_controller.go
@@ -25,12 +25,12 @@ import (
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -41,8 +41,9 @@ import (
 )
 
 const (
-	ObserverLabelKey = "observer.io/name"
-	FinalizerKey     = "observer.io/finalizer"
+	ObserverLabelKey    = "observer.io/name"
+	FinalizerKey        = "observer.io/finalizer"
+	InjectContainerName = "instrumentation"
 )
 
 // ObserverReconciler reconciles a Observer object
@@ -51,9 +52,12 @@ type ObserverReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=observer.io.observer.io,resources=observers,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=observer.io.observer.io,resources=observers/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=observer.io.observer.io,resources=observers/finalizers,verbs=update
+//+kubebuilder:rbac:groups=ob.observer.io,resources=observers,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=ob.observer.io,resources=observers/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=ob.observer.io,resources=observers/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -79,13 +83,13 @@ func (r *ObserverReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	observer := obj.DeepCopy()
 
 	if !observer.DeletionTimestamp.IsZero() {
-		return r.cleanResourcesForObserver(ctx, observer)
+		return r.cleanResources(ctx, observer)
 	}
 
 	return r.syncObserver(ctx, observer)
 }
 
-func (r *ObserverReconciler) cleanResourcesForObserver(ctx context.Context, observer *observerv1alpha1.Observer) (ctrl.Result, error) {
+func (r *ObserverReconciler) cleanResources(ctx context.Context, observer *observerv1alpha1.Observer) (ctrl.Result, error) {
 	if observer.Spec.Jaeger == nil {
 		return ctrl.Result{}, nil
 	}
@@ -126,32 +130,14 @@ func (r *ObserverReconciler) syncObserver(ctx context.Context, observer *observe
 	logger := log.FromContext(ctx)
 
 	var errs []error
-	if err := r.ensureJaeger(observer); err != nil {
+	if err := r.ensureJaeger(ctx, observer); err != nil {
 		logger.Error(err, "failed to ensure jaeger")
 		errs = append(errs, err)
 	}
 
-	initContainer := getInitContainer(observer.Spec.Launcher)
-	for _, selector := range observer.Spec.ResourceSelectors {
-		selector.Namespace = observer.Namespace
-		switch selector.Kind {
-		case "Deployment":
-			err := r.injectDeployment(ctx, initContainer, observer.Spec.Agent, selector)
-			if err != nil {
-				logger.Error(err, "failed to inject deployment", "selector", selector)
-				errs = append(errs, err)
-				continue
-			}
-		case "StatefulSet":
-			err := r.injectStatefulSet(ctx, initContainer, observer.Spec.Agent, selector)
-			if err != nil {
-				logger.Error(err, "failed to inject statefulset", "selector", selector)
-				errs = append(errs, err)
-				continue
-			}
-		default:
-			errs = append(errs, fmt.Errorf("unsupport resource kind %s", selector.Kind))
-		}
+	if err := r.ensureInjectAgent(ctx, observer); err != nil {
+		logger.Error(err, "failed to ensure inject agent")
+		errs = append(errs, err)
 	}
 
 	newStatus := observer.Status.DeepCopy()
@@ -192,7 +178,7 @@ func (r *ObserverReconciler) updateStatusIfNeed(ctx context.Context, observer *o
 	return nil
 }
 
-func (r *ObserverReconciler) ensureJaeger(observer *observerv1alpha1.Observer) error {
+func (r *ObserverReconciler) ensureJaeger(ctx context.Context, observer *observerv1alpha1.Observer) error {
 	if observer.Spec.Jaeger == nil {
 		return nil
 	}
@@ -209,89 +195,157 @@ func (r *ObserverReconciler) ensureJaeger(observer *observerv1alpha1.Observer) e
 	} else {
 		observer.Spec.Jaeger.Labels[ObserverLabelKey] = observer.Name
 	}
-	err := r.ensureJaegerDeployment(observer.Spec.Jaeger)
+	err := r.ensureJaegerDeployment(ctx, observer.Spec.Jaeger)
 	if err != nil {
 		return err
 	}
-	return r.ensureJaegerService(observer.Spec.Jaeger)
+	return r.ensureJaegerService(ctx, observer.Spec.Jaeger)
 }
 
-func (r *ObserverReconciler) ensureJaegerDeployment(jaegerConfig *observerv1alpha1.ObserverJaeger) error {
+func (r *ObserverReconciler) ensureJaegerDeployment(ctx context.Context, jaegerConfig *observerv1alpha1.ObserverJaeger) error {
 	deploy := newJaegerDeployment(jaegerConfig)
-	return r.createOrUpdateDeployment(deploy)
+	return r.createOrUpdateDeployment(ctx, deploy)
 }
 
-func (r *ObserverReconciler) ensureJaegerService(jaegerConfig *observerv1alpha1.ObserverJaeger) error {
+func (r *ObserverReconciler) ensureJaegerService(ctx context.Context, jaegerConfig *observerv1alpha1.ObserverJaeger) error {
 	svc := newJaegerService(jaegerConfig)
-	return r.createOrUpdateService(svc)
+	return r.createOrUpdateService(ctx, svc)
 }
 
-func (r *ObserverReconciler) injectDeployment(ctx context.Context, initContainer corev1.Container, agentConfig *observerv1alpha1.Agent, s observerv1alpha1.ResourceSelector) error {
-	deploy := &appv1.Deployment{}
-	if err := r.Client.Get(ctx, types.NamespacedName{Namespace: s.Namespace, Name: s.Name}, deploy); err != nil {
+func (r *ObserverReconciler) ensureInjectAgent(ctx context.Context, observer *observerv1alpha1.Observer) error {
+	logger := log.FromContext(ctx)
+	observer.Spec.Agent.Endpoint = observer.JaegerEndpoint()
+
+	var errs []error
+	for _, selector := range observer.Spec.ResourceSelectors {
+		selector.Namespace = observer.Namespace
+		switch selector.Kind {
+		case "Deployment":
+			if err := r.injectDeployment(ctx, observer, selector); err != nil {
+				logger.Error(err, "failed to inject for deployment")
+				errs = append(errs, err)
+				continue
+			}
+		case "StatefulSet":
+			if err := r.injectStatefulSet(ctx, observer, selector); err != nil {
+				logger.Error(err, "failed to inject for statefulset")
+				errs = append(errs, err)
+				continue
+			}
+		default:
+			errs = append(errs, fmt.Errorf("unsupport resource kind %s", selector.Kind))
+		}
+	}
+	return errors.NewAggregate(errs)
+}
+
+func (r *ObserverReconciler) injectDeployment(ctx context.Context, observer *observerv1alpha1.Observer, selector observerv1alpha1.ResourceSelector) error {
+	deploy := appv1.Deployment{}
+	if err := r.Client.Get(ctx, types.NamespacedName{Namespace: selector.Namespace, Name: selector.Name}, &deploy); err != nil {
 		return err
 	}
-	if len(deploy.Spec.Template.Spec.Containers) == 0 {
-		return fmt.Errorf("can not found any containers")
+	if isInjected(deploy.Spec.Template.Spec.Containers) {
+		return nil
 	}
-	agentContainer := getAgentContainer(agentConfig, deploy.Name, deploy.Spec.Template.Spec.Containers[0])
-	deploy.Spec.Template.Spec.InitContainers = append(deploy.Spec.Template.Spec.InitContainers, initContainer)
-	deploy.Spec.Template.Spec.Containers = append(deploy.Spec.Template.Spec.Containers, agentContainer)
-	return r.createOrUpdateDeployment(deploy)
-}
 
-func (r *ObserverReconciler) injectStatefulSet(ctx context.Context, initContainer corev1.Container, agentConfig *observerv1alpha1.Agent, s observerv1alpha1.ResourceSelector) error {
-	sts := &appv1.StatefulSet{}
-	if err := r.Client.Get(ctx, types.NamespacedName{Namespace: s.Namespace, Name: s.Name}, sts); err != nil {
+	initContainer := getInitContainer(observer.Spec.Launcher)
+	agentContainer := getAgentContainer(observer.Spec.Agent, deploy.Name, deploy.Spec.Template.Spec.Containers[0])
+	injectedPodTemplate, err := r.injectPodTemplateSpec(deploy.Spec.Template, initContainer, agentContainer)
+	if err != nil {
 		return err
 	}
-	if len(sts.Spec.Template.Spec.Containers) == 0 {
-		return fmt.Errorf("can not found any containers")
-	}
-	agentContainer := getAgentContainer(agentConfig, sts.Name, sts.Spec.Template.Spec.Containers[0])
-	sts.Spec.Template.Spec.InitContainers = append(sts.Spec.Template.Spec.InitContainers, initContainer)
-	sts.Spec.Template.Spec.Containers = append(sts.Spec.Template.Spec.Containers, agentContainer)
-	return r.createOrUpdateStatefulSet(sts)
+	deploy.Spec.Template = *injectedPodTemplate
+	return r.createOrUpdateDeployment(ctx, &deploy)
 }
 
-func (r *ObserverReconciler) createOrUpdateDeployment(deploy *appv1.Deployment) error {
+func (r *ObserverReconciler) injectStatefulSet(ctx context.Context, observer *observerv1alpha1.Observer, selector observerv1alpha1.ResourceSelector) error {
+	sts := appv1.StatefulSet{}
+	if err := r.Client.Get(ctx, types.NamespacedName{Namespace: selector.Namespace, Name: selector.Name}, &sts); err != nil {
+		return err
+	}
+	if isInjected(sts.Spec.Template.Spec.Containers) {
+		return nil
+	}
+	initContainer := getInitContainer(observer.Spec.Launcher)
+	agentContainer := getAgentContainer(observer.Spec.Agent, sts.Name, sts.Spec.Template.Spec.Containers[0])
+	injectedPodTemplate, err := r.injectPodTemplateSpec(sts.Spec.Template, initContainer, agentContainer)
+	if err != nil {
+		return err
+	}
+	sts.Spec.Template = *injectedPodTemplate
+	return r.createOrUpdateStatefulSet(ctx, &sts)
+}
+
+func (r *ObserverReconciler) injectPodTemplateSpec(tmpl corev1.PodTemplateSpec, initContainer corev1.Container, agentContainer corev1.Container) (*corev1.PodTemplateSpec, error) {
+	if len(tmpl.Spec.Containers) == 0 {
+		return nil, fmt.Errorf("can not found any containers")
+	}
+	tmpl.Spec.InitContainers = append(tmpl.Spec.InitContainers, initContainer)
+	tmpl.Spec.Containers = append(tmpl.Spec.Containers, agentContainer)
+	tmpl.Spec.Containers[0].Command = append([]string{"/odigos-launcher/launch"}, tmpl.Spec.Containers[0].Command...)
+	tmpl.Spec.Containers[0].VolumeMounts = append(
+		tmpl.Spec.Containers[0].VolumeMounts,
+		corev1.VolumeMount{
+			Name:      "launcherdir",
+			MountPath: "/odigos-launcher",
+		})
+	tmpl.Spec.Volumes = append(
+		tmpl.Spec.Volumes,
+		corev1.Volume{
+			Name: "launcherdir",
+			VolumeSource: corev1.VolumeSource{
+				EmptyDir: &corev1.EmptyDirVolumeSource{},
+			},
+		},
+		corev1.Volume{
+			Name: "kernel-debug",
+			VolumeSource: corev1.VolumeSource{
+				HostPath: &corev1.HostPathVolumeSource{
+					Path: "/sys/kernel/debug",
+				},
+			},
+		})
+	return &tmpl, nil
+}
+
+func (r *ObserverReconciler) createOrUpdateDeployment(ctx context.Context, deploy *appv1.Deployment) error {
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		got := appv1.Deployment{}
-		err := r.Client.Get(context.TODO(), types.NamespacedName{Namespace: deploy.Namespace, Name: deploy.Name}, &got)
+		err := r.Client.Get(ctx, types.NamespacedName{Namespace: deploy.Namespace, Name: deploy.Name}, &got)
 		if apierrors.IsNotFound(err) {
-			if err := r.Client.Create(context.TODO(), deploy); err != nil {
+			if err := r.Client.Create(ctx, deploy); err != nil {
 				return err
 			}
 		} else if err != nil {
 			return err
 		}
 		deploy.SetResourceVersion(got.GetResourceVersion())
-		return r.Client.Update(context.TODO(), deploy)
+		return r.Client.Update(ctx, deploy)
 	})
 }
 
-func (r *ObserverReconciler) createOrUpdateStatefulSet(sts *appv1.StatefulSet) error {
+func (r *ObserverReconciler) createOrUpdateStatefulSet(ctx context.Context, sts *appv1.StatefulSet) error {
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		got := appv1.StatefulSet{}
-		err := r.Client.Get(context.TODO(), types.NamespacedName{Namespace: sts.Namespace, Name: sts.Name}, &got)
+		err := r.Client.Get(ctx, types.NamespacedName{Namespace: sts.Namespace, Name: sts.Name}, &got)
 		if apierrors.IsNotFound(err) {
-			if err := r.Client.Create(context.TODO(), sts); err != nil {
+			if err := r.Client.Create(ctx, sts); err != nil {
 				return err
 			}
 		} else if err != nil {
 			return err
 		}
 		sts.SetResourceVersion(got.GetResourceVersion())
-		return r.Client.Update(context.TODO(), sts)
+		return r.Client.Update(ctx, sts)
 	})
 }
 
-func (r *ObserverReconciler) createOrUpdateService(svc *corev1.Service) error {
+func (r *ObserverReconciler) createOrUpdateService(ctx context.Context, svc *corev1.Service) error {
 	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		got := corev1.Service{}
-		err := r.Client.Get(context.TODO(), types.NamespacedName{Namespace: svc.Namespace, Name: svc.Name}, &got)
+		err := r.Client.Get(ctx, types.NamespacedName{Namespace: svc.Namespace, Name: svc.Name}, &got)
 		if apierrors.IsNotFound(err) {
-			if err := r.Client.Create(context.TODO(), svc); err != nil {
+			if err := r.Client.Create(ctx, svc); err != nil {
 				return err
 			}
 		} else if err != nil {
@@ -299,24 +353,8 @@ func (r *ObserverReconciler) createOrUpdateService(svc *corev1.Service) error {
 		}
 		svc.SetResourceVersion(got.GetResourceVersion())
 		svc.Spec.ClusterIP = got.Spec.ClusterIP
-		return r.Client.Update(context.TODO(), svc)
+		return r.Client.Update(ctx, svc)
 	})
-}
-
-func (r *ObserverReconciler) getMatchResources(ctx context.Context, selectors []observerv1alpha1.ResourceSelector) ([]*unstructured.Unstructured, error) {
-	var resources []*unstructured.Unstructured
-	var errs []error
-	for _, selector := range selectors {
-		applied := &unstructured.Unstructured{}
-		applied.SetAPIVersion(selector.APIVersion)
-		applied.SetKind(selector.Kind)
-		if err := r.Get(ctx, types.NamespacedName{Namespace: selector.Namespace, Name: selector.Name}, applied); err != nil {
-			errs = append(errs, err)
-			continue
-		}
-		resources = append(resources, applied)
-	}
-	return resources, errors.NewAggregate(errs)
 }
 
 func (r *ObserverReconciler) removeFinalizer(observer *observerv1alpha1.Observer) (ctrl.Result, error) {
@@ -352,6 +390,14 @@ func (r *ObserverReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&observerv1alpha1.Observer{}).
 		Complete(r)
+}
+
+func isInjected(containers []corev1.Container) bool {
+	nameSet := sets.Set[string]{}
+	for _, container := range containers {
+		nameSet.Insert(container.Name)
+	}
+	return nameSet.Has(InjectContainerName)
 }
 
 func newJaegerDeployment(jaegerConfig *observerv1alpha1.ObserverJaeger) *appv1.Deployment {
@@ -424,18 +470,9 @@ func newJaegerService(jaegerConfig *observerv1alpha1.ObserverJaeger) *corev1.Ser
 }
 
 func getInitContainer(launcherConfig *observerv1alpha1.Launcher) corev1.Container {
-	// set default config
-	if launcherConfig == nil {
-		launcherConfig = &observerv1alpha1.Launcher{
-			observerv1alpha1.Image{
-				ImageRepository: "keyval/launcher",
-				ImageTag:        "v0.1",
-			},
-		}
-	}
 	return corev1.Container{
 		Name:    "copy-launcher",
-		Image:   launcherConfig.Image.Name(),
+		Image:   launcherConfig.NameByDefault("keyval/launcher:v0.1"),
 		Command: []string{"cp", "-a", "/kv-launcher/.", "/odigos-launcher/"},
 		VolumeMounts: []corev1.VolumeMount{
 			{
@@ -448,7 +485,7 @@ func getInitContainer(launcherConfig *observerv1alpha1.Launcher) corev1.Containe
 
 func getAgentContainer(agentConfig *observerv1alpha1.Agent, name string, existContainer corev1.Container) corev1.Container {
 	return corev1.Container{
-		Name:  "instrumentation",
+		Name:  InjectContainerName,
 		Image: agentConfig.Image.Name(),
 		Env: []corev1.EnvVar{
 			{
